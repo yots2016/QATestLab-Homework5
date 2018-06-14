6d2947b18f8cb88e1c06eb71b721576b9c896522
Viacheslav Otamas <newstageja@gmail.com>
2018-06-14 15:42:34 +0300

    init commit

---
+++ src/test/java/myprojects/automation/assignment5/tests/PlaceOrderTest.java
@@ -0,0 +1,30 @@
+package myprojects.automation.assignment5.tests;
+
+import myprojects.automation.assignment5.BaseTest;
+import org.testng.annotations.Test;
+
+public class PlaceOrderTest extends BaseTest {
+
+    @Test
+    public void checkSiteVersion() {
+        // TODO open main page and validate website version
+    }
+
+    @Test
+    public void createNewOrder() {
+        // TODO implement order creation test
+
+        // open random product
+
+        // save product parameters
+
+        // add product to Cart and validate product information in the Cart
+
+        // proceed to order creation, fill required information
+
+        // place new order and validate order summary
+
+        // check updated In Stock value
+    }
+
+}
